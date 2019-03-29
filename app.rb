cd0f10e7c76bb96c4c532f5e95be977cef56eba8
Grace Choi <oiograceoio@gmail.com>
2019-03-29 20:32:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/reversename/:name' do 
+  get '/reversename/:name' do
     @name = params[:name]
-    @name.reverse 
+    @name.reverse
   end
 
 end
