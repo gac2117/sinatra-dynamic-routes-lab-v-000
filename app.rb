176e7bc7782d6a1a816cceb6b102a670dd4e6047
Grace Choi <oiograceoio@gmail.com>
2019-03-29 20:40:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,9 +13,9 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number].to_i 
+    @number = params[:number].to_i
     @phrase = params[:phrase]
-    @phrase * @number 
+    @phrase * @number
   end
 
 end
